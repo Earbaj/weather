b8f766cac5be13c93024071e8077fadd23b0f112
Earbaj <earbajsaria3@gmail.com>
2021-06-15 22:31:30 +0600

    Add network class

---
+++ lib/network/network_utils.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:weather/model/weather_forcast.dart';
+
+class Network {
+  Future<WeatherForcast> getWeatherForecast({required String cityName}) async{
+    var finalUrl = "https://api.openweathermap.org/data/2.5/forecast/daily?q="+cityName+
+        "&appid=14525566&units=imperial"; //change to metric or imperial
+
+    Response response = await get(Uri.parse(finalUrl));
+    print("URL: ${Uri.encodeFull(finalUrl)}");
+
+    if (response.statusCode == 200) {
+      // we get the actual mapped model ( dart object )
+      print("weather data: ${response.body}");
+      return WeatherForcast.fromJson(json.decode(response.body));
+    }else {
+      throw Exception("Error getting weather forecast");
+    }
+
+
+  }
+}
