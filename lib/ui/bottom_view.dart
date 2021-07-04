832a9352fae23f7582d48581ef82c5b3b13208c4
Earbaj <earbajsaria3@gmail.com>
2021-07-04 20:34:24 +0600

    Add rest of the bottom card text

--- lib/ui/bottom_view.dart
+++ lib/ui/bottom_view.dart
@@ -27,7 +27,7 @@ Widget bottomView(AsyncSnapshot<WeatherForcast> snapsot, BuildContext context) {
               return ClipRRect(
                 borderRadius: BorderRadius.all(Radius.circular(10.0)),
                 child: Container(
-                  width: MediaQuery.of(context).size.width / 2.7,
+                  width: MediaQuery.of(context).size.width / 2.2,
                   height: 160,
                   decoration: BoxDecoration(
                     gradient: LinearGradient(colors: [Color(0xff9661C3), Colors.white],
@@ -48,10 +48,18 @@ Widget bottomView(AsyncSnapshot<WeatherForcast> snapsot, BuildContext context) {
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           CircleAvatar(
-                            radius: 34.0,
+                            radius: 30.0,
                             backgroundColor: Colors.white,
                             child: getWeatherIcon(description: forcast[index].weather[0].main, color: Colors.blue, size: 34.0),
-                          )
+                          ),
+                          Column(
+                            children: [
+                              Text("Day : ${forcast[index].temp.day.toStringAsFixed(0)}°F"),
+                              Text("Max : ${forcast[index].temp.max.toStringAsFixed(0)}°F"),
+                              Text("Hum : ${forcast[index].humidity.toStringAsFixed(0)}%"),
+                              Text("Wind : ${forcast[index].speed.toStringAsFixed(1)}mi/h"),
+                            ],
+                          ),
                         ],
                       ),
                     ],
