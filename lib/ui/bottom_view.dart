f3348cd73f8a27fcdc7b0154770f7d7c7f891719
Earbaj <earbajsaria3@gmail.com>
2021-07-04 20:22:50 +0600

    Add Bottom view week and add circleAvater with icon

--- lib/ui/bottom_view.dart
+++ lib/ui/bottom_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:weather/model/weather_forcast.dart';
+import 'package:weather/ui/custome_weather_icon.dart';
 
 Widget bottomView(AsyncSnapshot<WeatherForcast> snapsot, BuildContext context) {
   var forcast = snapsot.data!.list;
@@ -12,11 +14,16 @@ Widget bottomView(AsyncSnapshot<WeatherForcast> snapsot, BuildContext context) {
           height: 170,
           padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
           child: ListView.separated(
+            scrollDirection: Axis.horizontal,
             itemCount: forcast.length,
             separatorBuilder: (context, index){
               return SizedBox(width: 8.0,);
             },
             itemBuilder: (context, index){
+              var daysofweek = "";
+              var fullDate = new DateTime.fromMillisecondsSinceEpoch(forcast[index].dt*1000);
+              var formated_date = DateFormat("EEEE, MMM d, y").format(fullDate);
+              daysofweek = formated_date.split(",")[0];
               return ClipRRect(
                 borderRadius: BorderRadius.all(Radius.circular(10.0)),
                 child: Container(
@@ -26,6 +33,28 @@ Widget bottomView(AsyncSnapshot<WeatherForcast> snapsot, BuildContext context) {
                     gradient: LinearGradient(colors: [Color(0xff9661C3), Colors.white],
                       begin: Alignment.topLeft, end: Alignment.bottomRight
                     ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(daysofweek),
+                          )
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            radius: 34.0,
+                            backgroundColor: Colors.white,
+                            child: getWeatherIcon(description: forcast[index].weather[0].main, color: Colors.blue, size: 34.0),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               );
